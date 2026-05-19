d7b3f8f92bc1ffbd9d959649ceaa6791c2e12e33
zeusog91-OG <zeusog91@gmail.com>
2026-05-20 00:18:10 +0530

    Update Page.tsx

--- app/app/Page.tsx
+++ app/app/Page.tsx
@@ -1,17 +1,1 @@
-import Navbar from '@/components/navbar'
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Pricing from '@/components/pricing'
-import Footer from '@/components/footer'
 
-export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-white dark:bg-gray-950">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Pricing />
-      <Footer />
-    </main>
-  )
-}
