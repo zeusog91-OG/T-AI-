aa930691afbf40cb11096dcf53a044b0c2f6ed9d
zeusog91-OG <zeusog91@gmail.com>
2026-05-20 12:51:11 +0530

    Create page.tsx

---
+++ app/Login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Login Page – Coming Soon
+      </h1>
+    </div>
+  )
+}
